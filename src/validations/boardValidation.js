47f2326aeb2035685ca955b619bfbaa6aba9195c
ImTheOne-9 <110813494+ImTheOne-9@users.noreply.github.com>
2025-05-21 21:14:10 +0700

    Handle controller layer

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,16 +1,17 @@
+/* eslint-disable no-console */
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
 
 const createNew = async (req, res, next) => {
   const correctCondition = Joi.object({
-    title: Joi.string().required().min(3).max(5).trim().strict().messages({
+    title: Joi.string().required().min(3).max(50).trim().strict().messages({
       'any.required': 'Title is required',
       'string.empty': 'Title is not allowed to be empty',
       'string.min': 'Title length must be at least 3 characters long',
-      'string.max': 'Title length must be less than or equal to 5 characters long',
+      'string.max': 'Title length must be less than or equal to 50 characters long',
       'string.trim': 'Title must not have leading or trailing whitespace',
     }),
-    description: Joi.string().required().min(3).max(5).trim().strict().messages({
+    description: Joi.string().required().min(10).max(200).trim().strict().messages({
       'any.required': 'Description is required',
       'string.empty': 'Description is not allowed to be empty',
       'string.min': 'Description length must be at least 10 characters long',
@@ -20,14 +21,13 @@ const createNew = async (req, res, next) => {
   })
 
   try {
-    console.log(req.body)
     await correctCondition.validateAsync(req.body, { abortEarly: false })
     next()
-    res.status(StatusCodes.CREATED).json({ message: 'POST from Validation: API create new board' })
   } catch (error) {
-    console.log('Error: ', error)
+    const errorDetails = error?.details?.map(err => err.message) || ['Validation failed']
+    console.log('Error: ', errorDetails)
     res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-      error: new Error(error).message
+      error: errorDetails
     })
   }
 }
