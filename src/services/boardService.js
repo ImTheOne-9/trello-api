5ab5ac1336c4e6ba5cf1f2363121fb933699e464
ImTheOne-9 <110813494+ImTheOne-9@users.noreply.github.com>
2025-05-24 16:33:06 +0700

    Create resBoard aggregate

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-useless-catch */
 import { StatusCodes } from 'http-status-codes'
+import { cloneDeep } from 'lodash'
 import { boardModel } from '~/models/boardModel'
 import ApiError from '~/utils/ApiError'
 import { slugify } from '~/utils/slugToString'
@@ -28,7 +29,17 @@ const getDetails = async (boardId) => {
   try {
     const board = await boardModel.getDetails(boardId)
     if (!board) throw new ApiError(StatusCodes.NOT_FOUND, 'Not found board!')
-    return board
+
+    //Clone board
+    const resBoard = cloneDeep(board)
+
+    // Dieu chinh lai data tra ve
+    resBoard.columns.forEach(column => {
+      column.cards = resBoard.cards.filter(card => card.columnId.equals(column._id))
+    })
+
+    delete resBoard.cards
+    return resBoard
   } catch (error) {
     throw error
   }
