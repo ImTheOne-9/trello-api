2fdfa630b4345011ddf3ba245c5825e55e67da18
ImTheOne-9 <110813494+ImTheOne-9@users.noreply.github.com>
2025-05-24 13:28:41 +0700

    Config Cors Policy

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,1 +1,27 @@
+import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
+import { WHITELIST_DOMAIN } from '~/utils/constants'
+import { env } from './environment'
 
+export const corsOptions = {
+  origin: function (origin, callback) {
+    // Cho phep viec goi API bang POSTMAN trong moi truong Dev
+    if (!origin && env.BUILD_MODE === 'development') {
+      return callback(null, true)
+    }
+
+    // Kiem tra origin co phai Domain duoc chap nhan khong
+    if (WHITELIST_DOMAIN.includes(origin)) {
+      return callback(null, true)
+    }
+
+
+    return callback(new ApiError(`${origin} not allowed by our CORS Policy`, StatusCodes.FORBIDDEN))
+  },
+
+  // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200,
+
+  //Cors se cho phep nhan cookies tu request (Dinh kiem jwt access token va refresh token vao httpOnly Cookies)
+  credentials: true
+}
