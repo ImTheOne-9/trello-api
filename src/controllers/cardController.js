e8be7a80387a9efc8436348a1700343c44a1df26
ImTheOne-9 <110813494+ImTheOne-9@users.noreply.github.com>
2025-06-05 19:05:07 +0700

    Edit Active Card Detail & Display member

--- src/controllers/cardController.js
+++ src/controllers/cardController.js
@@ -11,7 +11,21 @@ const createNew = async (req, res, next) => {
   }
 }
 
+const update = async (req, res, next) => {
+  try {
+    const cardId = req.params.id
+    const cardCoverFile = req.file
+    // Dieu huong sang tang Service
+    const updatedCard = await cardService.update(cardId, req.body, cardCoverFile)
+    // Co ket qua thi tra ve phia client
+    res.status(StatusCodes.CREATED).json(updatedCard)
+  } catch (error) {
+    next(error)
+  }
+}
+
 
 export const cardController = {
-  createNew
+  createNew,
+  update
 }
--- src/validations/cardValidation.js
+++ src/validations/cardValidation.js
@@ -18,6 +18,24 @@ const createNew = async (req, res, next) => {
   }
 }
 
+const update = async (req, res, next) => {
+  const correctCondition = Joi.object({
+    title: Joi.string().min(3).max(50).trim().strict(),
+    description: Joi.string().optional()
+  })
+
+  try {
+    await correctCondition.validateAsync(req.body, {
+      abortEarly: false,
+      allowUnknown: true
+    })
+    next()
+  } catch (error) {
+    next(new ApiError(new Error(error).message, StatusCodes.UNPROCESSABLE_ENTITY))
+  }
+}
+
 export const cardValidation = {
-  createNew
+  createNew,
+  update
 }
